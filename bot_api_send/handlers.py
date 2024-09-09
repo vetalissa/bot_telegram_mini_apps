0ea6f75ae93cc4588b2528f5d86a1988d0c5f054
Elizaveta Dostovalova <lizakot20@gmail.com>
2024-09-09 17:20:23 +0300

    create new description

--- bot_api_send/handlers.py
+++ bot_api_send/handlers.py
@@ -9,23 +9,21 @@ from aiogram.methods import SendPhoto, SendVideo
 from aiogram.types import Message
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.utils.markdown import hbold
-
 from loader import dp
 
-PARAMETER_SENDER = 'CAT'
+PARAMETER_SENDER = 'CAT' # Параметр отображения
 
 
-# Декоратор регистрирует этот обработчик для команды /start.
 @dp.message(CommandStart())
 async def cmd_start(message: Message) -> None:
-    """
-    Отправила ответное сообщение с приветствием, включая полное имя пользователя в жирном стиле.
-    """
     await message.answer(f'Привет, {hbold(message.from_user.full_name)}!')
 
 
 @dp.message(Command('menu'))
 async def menu(message: types.Message):
+    """
+    Навигация для выбора.
+    """
     builder = InlineKeyboardBuilder()
     builder.add(types.InlineKeyboardButton(
         text='получить ответ на вопрос, да или нет?',
@@ -42,6 +40,9 @@ async def menu(message: types.Message):
 
 @dp.callback_query(F.data == 'yes_or_no')
 async def change_yesorno_parameter(callback: types.CallbackQuery):
+    """
+     Заменяет параметра на показ гиф мемов
+    """
     global PARAMETER_SENDER
     PARAMETER_SENDER = 'YESORNO'
     await callback.message.answer('Напиши свой вопрос, ответ да или нет....')
@@ -50,6 +51,9 @@ async def change_yesorno_parameter(callback: types.CallbackQuery):
 
 @dp.callback_query(F.data == 'cat_img')
 async def change_cat_parameter(callback: types.CallbackQuery):
+    """
+     Заменяет параметра на показ картинок котиков
+    """
     global PARAMETER_SENDER
     PARAMETER_SENDER = 'CAT'
     await callback.message.answer('Ты хочешь посмотреть на котиков?')
@@ -58,6 +62,9 @@ async def change_cat_parameter(callback: types.CallbackQuery):
 
 @dp.message()
 async def answer_for_message(message: types.Message) -> None:
+    """
+     Функция отдает, рандомную картинку или гифку
+    """
     try:
         if PARAMETER_SENDER == 'YESORNO':
             API_MEM_URL = 'https://yesno.wtf/api?force=no?yes'
@@ -73,6 +80,6 @@ async def answer_for_message(message: types.Message) -> None:
             if cat_response.status_code == 200:
                 cat_link = cat_response.json()[0]['url']
                 await message.bot(SendPhoto(chat_id=message.chat.id, photo=cat_link))
-
+                await message.answer('Еще котиков? ответ нет не принимается')
     except TypeError:
         await message.answer('Технические шоколадки!')
